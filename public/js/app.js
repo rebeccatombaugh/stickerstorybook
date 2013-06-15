4d84dbe73076525fc3671f546929b0f959093a18
Caleb Cauthon <calebcauthon@gmail.com>
2013-06-15 12:26:30 -0500

    made the stickers able to be peeled over and over

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-  $('.sticker.draggable').draggable();
+
+  $('.sticker').each(function() {
+    var sticker_stack = $(this);
+    
+    var make_a_new_sticker = function() {
+      var fresh_sticker = $(sticker_stack).find('.permanent').clone().removeClass('permanent').addClass('draggable').draggable({
+        stop: make_a_new_sticker
+      }).css('position', '');
+      $(sticker_stack).append(fresh_sticker)    
+    };
+    
+    
+    
+    $(this).find('.draggable').draggable({
+      stop: make_a_new_sticker  
+    });
+  });
 
 });
