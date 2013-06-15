06557391cb2709cbfa2d0783a49e812598774ec9
Caleb Cauthon <calebcauthon@gmail.com>
2013-06-15 12:43:06 -0500

    added 'dropped' css class marker

--- public/js/app.js
+++ public/js/app.js
@@ -4,19 +4,31 @@ $(document).ready(function() {
   $('.sticker').each(function() {
     var sticker_stack = $(this);
     
+    var on_drag_stop = function(params) {      
+      params.sticker.addClass('dropped');
+      add_a_new_sticker_to_the_top_of_the_stack();
+    };
+    
     var add_a_new_sticker_to_the_top_of_the_stack = function() {
       
       var fresh_sticker = $(sticker_stack).find('.permanent').clone().removeClass('permanent').addClass('draggable');
       fresh_sticker.draggable({
-        stop: add_a_new_sticker_to_the_top_of_the_stack
+        stop: function() {                    
+          on_drag_stop({ sticker: fresh_sticker });
+        }
       });
       
       $(sticker_stack).append(fresh_sticker)    
     };
     
-    $(this).find('.draggable').draggable({
-      stop: add_a_new_sticker_to_the_top_of_the_stack  
+    $(this).find('.draggable').each(function() {
+      var sticker = $(this);
+      sticker.draggable({
+      
+        stop: function() {                    
+          on_drag_stop({ sticker: sticker });
+        }
+      });
     });
   });
-
 });
