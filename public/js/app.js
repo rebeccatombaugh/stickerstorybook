513b9d9d61e9a0404757d47d821aac6e6ca858a6
Darren Cauthon <darren@cauthon.com>
2013-06-15 12:37:56 -0500

    Progress organizing the data we will need.

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 
   $('.sticker.draggable').draggable({
     stop: function() {
-            console.log($(this).position().top);
-            console.log($(this).position().left);
+            var data = {
+                         top: $(this).position().top,
+                         left: $(this).position().left,
+                         classList: $(this).attr('class').split(/\s+/)
+                       }
+            //$.jStorage.set("mykey", "keyvalue");
+            console.log(data);
           }
   });
 
