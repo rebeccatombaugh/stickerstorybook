8b81e610ee56143c5560ac3957807bf5f1c2703b
Darren Cauthon <darren@cauthon.com>
2013-06-15 13:17:38 -0500

    Remove code that will conflict.

--- public/js/app.js
+++ public/js/app.js
@@ -1,31 +1,3 @@
-function save_drag_points(){
-  var data = {
-               top: $(this).css('top'),
-               left: $(this).css('left'),
-               classList: $(this).attr('class').split(/\s+/)
-             }
-  console.log(data);
-  $.jStorage.set("mykey", data);
-}
-
-function load_the_data(){
-  data = $.jStorage.get("mykey");
-
-  if (data != undefined) {
-    var div = $('<div><div class="draggable"></div></div>');
-
-    $(data['classList']).each(function(index, value){
-      div.addClass(value);
-    });
-
-    div.css('position', 'relative');
-    div.css('left',     data['left']);
-    div.css('top',      data['top']);
-
-    $('.sticker_pen').append(div);
-  }
-}
-
 $(document).ready(function() {
   load_the_data();
 
