d2ac94b5ed378d321de7513c595221f6428da2f6
Darren Cauthon <darren@cauthon.com>
2013-06-15 12:57:20 -0500

    Progress with saving state.

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
 
+
+  data = $.jStorage.get("mykey");
+
+  if (data != undefined) {
+    var div = $('<div><div class="draggable"></div></div>');
+
+    $(data['classList']).each(function(index, value){
+      div.addClass(value);
+    });
+
+    div.css('position', 'relative');
+    div.css('left',     data['left']);
+    div.css('top',      data['top']);
+
+    $('.sticker_pen').append(div);
+  }
+
   $('.sticker.draggable').draggable({
     stop: function() {
             var data = {
-                         top: $(this).position().top,
-                         left: $(this).position().left,
+                         top: $(this).css('top'),
+                         left: $(this).css('left'),
                          classList: $(this).attr('class').split(/\s+/)
                        }
-            //$.jStorage.set("mykey", "keyvalue");
-            console.log(data);
+            $.jStorage.set("mykey", data);
           }
   });
+
+
+
 
 });
