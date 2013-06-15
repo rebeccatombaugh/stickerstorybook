848327a5a222f40304f8f251b4019eac95f032b4
Darren Cauthon <darren@cauthon.com>
2013-06-15 16:06:54 -0500

    Refactor images to a list.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'thin'
 gem 'haml'
+gem 'subtle'
 
 group 'development' do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
+require 'subtle'
 require 'sinatra/base'
 
 class App < Sinatra::Base
+
+  before do
+    @images = [:name, :type].to_objects {[
+               ['elephant', 'wide'],
+               ['apple',    'tall']
+              ]}
+  end
+
   get '/' do
     erb :edit
   end 
