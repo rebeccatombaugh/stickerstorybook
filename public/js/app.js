d8ae8b2f42190b8494ab76d1811d7eaac5ccde90
Caleb Cauthon <calebcauthon@gmail.com>
2013-06-15 12:33:43 -0500

    refactored and renamed some variables

--- public/js/app.js
+++ public/js/app.js
@@ -4,17 +4,18 @@ $(document).ready(function() {
   $('.sticker').each(function() {
     var sticker_stack = $(this);
     
-    var make_a_new_sticker = function() {
-      var fresh_sticker = $(sticker_stack).find('.permanent').clone().removeClass('permanent').addClass('draggable').draggable({
-        stop: make_a_new_sticker
-      }).css('position', '');
+    var add_a_new_sticker_to_the_top_of_the_stack = function() {
+      
+      var fresh_sticker = $(sticker_stack).find('.permanent').clone().removeClass('permanent').addClass('draggable');
+      fresh_sticker.draggable({
+        stop: add_a_new_sticker_to_the_top_of_the_stack
+      });
+      
       $(sticker_stack).append(fresh_sticker)    
     };
     
-    
-    
     $(this).find('.draggable').draggable({
-      stop: make_a_new_sticker  
+      stop: add_a_new_sticker_to_the_top_of_the_stack  
     });
   });
 
